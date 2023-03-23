74ea1bf8e645314e1a6dcd31a1511f18d02e5d5e
radmanlo <radman.lotfiazar@gmail.com>
2023-03-23 19:22:06 +0300

    Array is started and its initialization is explained

---
+++ Array/array.js
@@ -0,0 +1,30 @@
+/*
+    Array Initialization
+*/
+
+//create an empty array
+const arr1 = new Array()
+console.log(arr1)
+
+//create an array with 5 empty items
+const arr2 = new Array(5);
+console.log(arr2)
+
+//create an array with 5 specified items
+const arr3 = new Array(1,2,"BMW", "Java", 5 )
+console.log(arr3)
+
+//create an array with 1 item 
+const arr4 = new Array("1")
+console.log(arr4)
+
+//create an empty array 
+const arr5 = []
+console.log(arr5)
+
+//create an array with 5 specified items
+const arr6 = ["Python", 22, "JavaScript", 56, "a"]
+console.log(arr6)
+
+//cr
+
