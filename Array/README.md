8a368c56725485ae545a54808dc79f4657d4b42f
radmanlo <radman.lotfiazar@gmail.com>
2023-04-06 19:26:55 +0300

    Array is done

--- Array/README.md
+++ Array/README.md
@@ -8,3 +8,44 @@ const arr4 = new Array("1")<br />
 const arr5 = []<br />
 const arr6 = ["Python", 22, "JavaScript", 56, "a"]<br />
 
+# Array Property
+
+const length = array.length
+
+# Array Methods
+
+const reversedArray = array.reverse()<br />
+const sortedArray = array.sort()<br />
+const filedArr = array.fill(value, start, end)<br />
+const joinedString = array.join(separator)<br />
+const stringOfArray = array.toString()<br />
+const removedElement = array.pop()<br />
+const shifedElement = array.shift()<br />
+const IndexOfPushedElement = array.push(element)<br />
+const unshifedElement = arrTemp.unshift(element)<br />
+const concatArray = array1.concat(arrayw)<br />
+const spliceArray = array.splice(start, deletCount, addItem1, ... , addItemN)<br />
+const lastIndex = array.lastIndexOf(element)<br />
+const firstIndex = array.indexOf(element)<br />
+const sliceArray = array.slice( start, end)<br />
+const firstIndex = arr.findIndex(callback(element, index, arr),thisArg)<br />
+const value = array.find(callback(element, index, arr),thisArg)<br />
+const boolInclude = arraray.includes(element)<br />
+const resultReduceRight = array.reduceRight(callback(accumulator, currentValue), initialValue)<br />
+const resultReduce = array.reduce(callback(accumulator, currentValue), initialValue)<br />
+const boolIsArray = Array.isArray(array)<br />
+const newArrayFilter = array.filter(callback(element), thisArg)<br />
+const newArrayMap = arr.map(callback(currentValue), thisArg)<br />
+array.forEach(function(currentValue, index, arr), thisValue)<br />
+const boolSome = array.some(callback(currentValue), thisArg)<br />
+const boolEvery = array.every(function(currentValue, index, arr), thisValue)<br />
+const newArrayEntries = array.entries()<br />
+const newArrayKes = array.keys()<br />
+const newArrayValues = array.values()<br />
+const newArrayFrom = Array.from(arraylike, mapFunc, thisArg)<br />
+array.copyWithin(target, start, end)<br />
+const newArrayFlat = array.flat(depth)<br />
+
+# Spread Operator
+
+const arrCopy = [...array]<br />
--- Array/array.js
+++ Array/array.js
@@ -34,26 +34,32 @@ let alphabets = Array.of("A", "B", "C");
 
 
 /*
-    Array Methods
+    Array Property
 */
 
 // find the length of array
 const length =  arr3.length
 // console.log( `length of arr3 is ${length}`) //=> length of arr3 is 5
 
+
+
+/*
+    Array Methods
+*/
+
 // reversing the array
-const reversedArr3 = arr3.reverse();
+const reversedArr3 = arr3.reverse()
 // console.log(arr3) //=> [ 5, 'Java', 'BMW', 2, 1 ]
 // console.log(reversedArr3) //=> [ 5, 'Java', 'BMW', 2, 1 ]
 
 // sort the city array in ascending order
-const city = ["California", "Barcelona", "Paris", "Kathmandu"];
-let sortedArray = city.sort();
+const city = ["California", "Barcelona", "Paris", "Kathmandu"]
+let sortedArray = city.sort()
 // console.log(sortedArray) //=> [ 'Barcelona', 'California', 'Kathmandu', 'Paris' ]
 // console.log(city) //=> [ 'Barcelona', 'California', 'Kathmandu', 'Paris' ]
 
 // filling every element of the array with 'Cherry' 
-const filedArr = arr6.fill("Cherry");
+const filedArr = arr6.fill("Cherry")
 // console.log(arr6) //=> [ 'Cherry', 'Cherry', 'Cherry', 'Cherry', 'Cherry' ]
 // console.log(filedArr) //=> [ 'Cherry', 'Cherry', 'Cherry', 'Cherry', 'Cherry' ]
 
@@ -63,7 +69,7 @@ const filedArr2 = arr6.fill(15, 2, 4)
 // console.log(arr6) //=> [ 'Cherry', 'Cherry', 15, 15, 'Cherry' ]
 
 // join all elements of array using space  .join(separator)
-const message = ["JavaScript", "is", "fun."];
+const message = ["JavaScript", "is", "fun."]
 const joinedMessage = message.join(" ")
 // console.log(joinedMessage) //=> JavaScript is fun.
 // console.log(message) //=> [ 'JavaScript', 'is', 'fun.' ]
@@ -109,7 +115,7 @@ const concatMusic = music1.concat(music2)
 const num = [1,2,3,4,5,6,7,8]
 const spliceReturn = num.splice(1, 4, 99, 88)
 // console.log(spliceReturn) //=> [ 2, 3, 4, 5 ]
-// console.log(num) //=>  1, 99, 88, 6, 7, 8 ]
+// console.log(num) //=>  [1, 99, 88, 6, 7, 8 ]
 
 // return the index of the last occurance of a specified element in parameter arr.lastIndexOf( element, startFromIndex)
 const repeatNum = [1,2,1,3,1,4,1,5,1,6,7]
@@ -142,7 +148,7 @@ const findIndexReturn = arr.findIndex( (element, index, arr) => {
 })
 // console.log(findIndexReturn) //=> 1
 
-// return the index of the first element which satisfy the callback function condition arr.find(callback(element, index, arr),thisArg) 
+// return the value of the first element which satisfy the callback function condition arr.find(callback(element, index, arr),thisArg) 
 arr = [2, 4, 6, 11, 13]
 const findReturn = arr.find( (element, index, arr) => {
     // console.log(index)     //=> 0                  //=> 1                  //=> 2                  //=> 3 
@@ -214,6 +220,7 @@ const resultMap = arr.map(element=>{
 // console.log(resultMap) //=> [ 1, 2, 3, 4, 5, 6 ]
 
 // for each elemet in the array execute the callback function
+// array.forEach(function(currentValue, index, arr), thisValue)
 arr = ["Apple", "Orange", "Banana", "strawberry"]
 arr.forEach (element =>{
     // console.log(element)   //=> Apple    //=> Orange    //=> Orange    //=> strawberry
@@ -226,6 +233,7 @@ const resultSome = arr.some (element=> element % 2 === 0)
 // console.log(resultSome)  //=> true
 
 // return true if all elements in the array pass the condition defiend by callback function
+// array.every(function(currentValue, index, arr), thisValue)
 arr = [1, 2, 3, 4, "Apple", 8, 9, 10]
 const resultEvery = arr.every (element => Number.isInteger(element))
 // console.log (resultEvery)  //=> false
@@ -262,21 +270,26 @@ const resultValues = arr.values()
 const resultFrom = Array.from("javascript", element => element.toUpperCase() )
 // console.log(resultFrom)   //=> [ 'J', 'A', 'V', 'A', 'S', 'C', 'R', 'I', 'P', 'T' ]
 
+arr = ["Java", "Ruby", "JavaScript", "C++", "TypeScript"]
 // copies array elements from  one position to another (it does not return new array it change the original array)
 // arr.copyWithin(target, start, end)
-arr = ["Java", "Ruby", "JavaScript", "C++", "TypeScript"]
 const resultCopyWithin = arr.copyWithin(0, 2, 4)
 // console.log(arr)                 //=> [ 'JavaScript', 'C++', 'JavaScript', 'C++', 'TypeScript' ]
 // console.log(resultCopyWithin)   //=> [ 'JavaScript', 'C++', 'JavaScript', 'C++', 'TypeScript' ]
 
+// create and return a new array by flattening a nested array up to specified depth
+arr = [1, 0, 5, [45, 55, 98, [155, 286, 398, [4567, 2589]]]]
+const resultFlat = arr.flat(2)
+// console.log(resultFlat)   //=> [ 1, 0, 5, 45, 55, 98, 155, 286, 398, [ 4567, 2589 ] ]
 
-
-
-
-
-
-
-
-// https://www.programiz.com/javascript/library/array#:~:text=In%20JavaScript%2C%20Array%20is%20a,default%20or%20custom%20sorting%20rules.
-
+/*
+// Spreed operator
+// this used for deep copy of an array
+ */
+arr = ["Java", "Python", "JavaScript", "GO"]
+const arrCopy = [...arr]
+arrCopy[0] = "Roby"
+arrCopy[2] = 0.8555
+// console.log(arr)        //=> [ 'Java', 'Python', 'JavaScript', 'GO' ]
+// console.log(arrCopy)   //=> [ 'Roby', 'Python', 0.8555, 'GO' ]
 
